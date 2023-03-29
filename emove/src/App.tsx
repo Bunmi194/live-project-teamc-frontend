4edd3602b10f4b5e426306d6dd725dac8314e151
Femi Denton <femidenton@yahoo.co.uk>
2023-03-29 11:33:19 +0100

    changes

--- emove/src/App.tsx
+++ emove/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ForgotPassword from './pages/forgotPassword';
+import CheckEmail from './pages/checkEmail';
 // import tw from "tailwind-styled-components";
 
-function App() {
+function App() { 
   return (
     <Routes>
       <Route path="/" element={<Home />} ></Route>
+      <Route path="/forgotPassword" element={<ForgotPassword />} ></Route>
+      <Route path="/checkemail" element={<CheckEmail />} ></Route>
     </Routes>
   );
 }
